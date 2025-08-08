e25ca9f59b5217c5f4e17f163cff83f2106b930f
Luke Forte <lf770@bath.ac.uk>
2025-08-08 16:33:00 +0100

    update

--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,6 @@
 # config the api keys and endpoints
 
-ALPACA_API_KEY = "PKP6GSESOYXX7PB7V433"
-ALPACA_SECRET_KEY = "5bt8LNjgbE1x4GMmpsv3E1hQrD3QvY5RqpYoAr4k"
+API_KEY = "PKZJQ3BB1CBWGPIZ5HBQ"
+API_SECRET = "OjGLPPzgO49jjbCPrTU7GZi3IsPoPIlFBIyPECbh"
 
-API_BASE_URL = ""
+API_BASE_URL = "https://paper-api.alpaca.markets/v2"
--- bot/data_handler.py
+++ bot/data_handler.py
@@ -1,1 +1,30 @@
 
+
+
+from config import API_KEY, API_SECRET, API_BASE_URL
+import pandas as pd
+import alpaca_trade_api as tradeapi
+from alpaca_trade_api.rest import TimeFrame
+
+api = tradeapi.REST(API_KEY, API_SECRET, API_BASE_URL, api_version='v2')
+
+# Define symbols and date range
+symbols_to_fetch = ['AAPL', 'GOOG']
+start_date_iso = '2023-01-01T00:00:00Z' # Use ISO 8601 format
+end_date_iso = '2024-01-01T00:00:00Z'
+
+# Use the get_bars() method to retrieve historical data
+print(f"Fetching daily bars for {', '.join(symbols_to_fetch)}...")
+
+barset = api.get_bars(
+    symbols_to_fetch,
+    TimeFrame.Day,
+    start=start_date_iso,
+    end=end_date_iso,
+    adjustment='raw'
+)
+
+# The .df property converts the BarSet object into a pandas DataFrame
+df = barset.df
+# Display the first few rows of the DataFrame
+print(df.head())
